a1574fc6c71c1bb620ee0598534b892e525a09ec
jfranco3 <jfranco376@gmail.com>
2022-07-28 11:22:34 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false, todos: [], text: "" };
+  }
+
+  handleOnChange = (e) => {
+    //how to change value
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  onClickHandler = (e) => {
+    //populate array of todos with text
+    console.log(this.state.text);
+    //push()
+    const text = this.state.text;
+    const newState = [...this.state.todos];
+    console.log(newState);
+    newState.push(text);
+    console.log("NEW STATE", newState); //["text is here now"]
+    //set the state
+    this.setState({
+      text: "",
+      todos: newState,
+    });
+  };
+
+  handleClick = (index) => {
+    console.log("INDEX", index);
+    this.delete(index);
+  };
+
+  delete = (index) => {
+    console.log("THIS WILL BE DELETED", index);
+    const newTodos = [...this.state.todos, index];
+    newTodos.splice(index, 1);
+    //update state
+    this.setState({
+      todos: newTodos,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ToDo</h1>
+        <input value={this.state.text} onChange={this.handleOnChange} />
+        <p>{this.state.text}</p>
+        <button onClick={this.onClickHandler}>ADD ITEM</button>
+        {this.state.todos.map((item, i) => (
+          <li key={i} onClick={() => this.handleClick(i)}>
+            {item}
+          </li>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
